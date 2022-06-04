5a38ad8a36761496baf6135d6b4709b593d865c2
Jennifer Byrnes <jennifer.byrnes@outlook.com>
2022-06-04 10:48:11 -0400

    Initial JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+  window.alert("The fight ahas begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation mards around playerName
+
+console.log(playerName);
+console.log("This  logs a string, good for leaving yourself a message");
+// this does math
+console.log(10+10);
+// ?
+console.log("Our robot's name is " + playerName);
+
+// fight();
